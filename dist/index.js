95f91c19f97ab0e4ff97a3b2153381f6731584c1
Jack Thompson <jacktinternetboy@gmail.com>
2016-08-27 12:35:33 -0400

    uh

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+var Robot = (function () {
+    function Robot(greet) {
+        this.greeting = greet;
+    }
+    Robot.prototype.say = function () {
+        console.log(this.greeting);
+    };
+    Robot.prototype.shoot = function (gun) {
+        gun.bullets;
+        console.log("SPLAT");
+    };
+    return Robot;
+}());
+var RobotGun = (function () {
+    function RobotGun(bullets, type) {
+        this.bullets = bullets;
+        this.type = type;
+    }
+    return RobotGun;
+}());
+var meat = new Robot("plz daddy not the whip");
+meat.say();
+meat.shoot(new RobotGun(50, "cats"));
---
+++ index.ts
@@ -0,0 +1,30 @@
+interface Gun {
+    bullets: number;
+    type?: "auto" | "cats"
+}
+
+class Robot {
+    greeting: string;
+    constructor (greet: string) {
+        this.greeting = greet;
+    }
+
+    public say() {
+        console.log(this.greeting);
+    }
+
+    public shoot(gun: Gun) {
+        gun.bullets;
+        console.log("SPLAT");
+    }
+}
+
+class RobotGun {
+    constructor (public bullets: number, public type: "auto" | "cats") {
+ 
+    }
+}
+
+var meat = new Robot("plz daddy not the whip");
+meat.say();
+meat.shoot(new RobotGun(50, "cats"));
